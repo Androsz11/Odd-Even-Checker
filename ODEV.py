5d83a217ce1642853356270650519fc115f79b7f
Androsz11 <73430696+Androsz11@users.noreply.github.com>
2020-10-28 14:36:59 +0530

    Add files via upload

---
+++ ODEV.py
@@ -0,0 +1,10 @@
+print("ODD EVEN Calculator")
+print("Scripted by Hritabhas Modak")
+print
+print
+No=int(input("Enter the Number"));
+if(No%2==0):
+    print(No,"is an even number");
+else:
+    print(No,"is an odd number");
+    
